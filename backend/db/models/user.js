5895e8627423bd55a1e4d66d011f49cad96cf3b7
bssam <bssammel@gmail.com>
2024-04-16 19:18:30 -0400

    woohoo db with users

---
+++ backend/db/models/user.js
@@ -0,0 +1,74 @@
+'use strict';
+const {
+  Model, Validator
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    email: {
+      type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+    },
+    phone:{
+      type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [10, 10],
+          isNumeric: true
+        }
+    },
+    firstName:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 255],
+        isAlpha: true
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 255],
+        isAlpha: true
+      }
+    },
+    imageURL: {
+      type: DataTypes.STRING,
+    },
+    providerBool:{ 
+      type:DataTypes.BOOLEAN,
+      allowNull:false
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope:{
+      attributes:{
+        exclude:["hashedPassword","createdAt","updatedAt"]//protecting sensitive information
+      }
+    }
+  });
+  return User;
+};
---
+++ backend/db/seeders/20240416225034-demo-user.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require("bcryptjs")
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; 
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        email:'simonkrawa@simon.site',
+        phone:'3524155409',
+        firstName: 'Simon',
+        lastName: 'Krawa',
+        imageURL:'https://i.imgur.com/Dapg8Ng.jpeg',
+        providerBool:false,
+        hashedPassword: bcrypt.hashSync('simonPass')
+      },
+      {
+        email:'keirannkrawa@simon.site',
+        phone:'4077937788',
+        firstName: 'Keiran',
+        lastName: 'Krawa',
+        imageURL:'https://i.pinimg.com/564x/14/43/55/144355d7b36c5f646435423798281ce9.jpg',
+        providerBool:true,
+        hashedPassword: bcrypt.hashSync('keiranPass')
+      },
+      {
+        email:'kim@simon.site',
+        phone:'1234567890',
+        firstName: 'Kimberly',
+        lastName: 'Kozlowski',
+        providerBool:true,
+        hashedPassword: bcrypt.hashSync('kimPass')
+      },
+      {
+        email:'sean@simon.site',
+        phone:'2345678901',
+        firstName: 'Sean',
+        lastName: 'Kozlowski',
+        providerBool:false,
+        hashedPassword: bcrypt.hashSync('seanPass')
+      },
+      {
+        email:'archie@simon.site',
+        phone:'3456789012',
+        firstName: 'Monarch',
+        lastName: 'Lamerson',
+        providerBool:false,
+        hashedPassword: bcrypt.hashSync('archiePass')
+      },
+      {
+        email:'lucas@simon.site',
+        phone:'4567890123',
+        firstName: 'Lucas',
+        lastName: 'Barrs',
+        providerBool:false,
+        hashedPassword: bcrypt.hashSync('lucasPass')
+      },
+      {
+        email:'provider@simon.site',
+        phone:'5678901234',
+        firstName: 'Demo',
+        lastName: 'Provider',
+        providerBool:true,
+        hashedPassword: bcrypt.hashSync('providerPass')
+      },
+      {
+        email:'patient@simon.site',
+        phone:'6789012345',
+        firstName: 'Demo',
+        lastName: 'Patient',
+        providerBool:false,
+        hashedPassword: bcrypt.hashSync('providerPass')
+      },
+     ],{ validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      email: { [Op.in]: ['simonkrawa@simon.site','keirannkrawa@simon.site','kim@simon.site','sean@simon.site','archie@simon.site','lucas@simon.site','provider@simon.site','patient@simon.site']}
+    }, {});
+  }
+};
