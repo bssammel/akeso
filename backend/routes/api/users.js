7942574a13ba2c094ece8d8852cf14f0f0263cf6
bssam <bssammel@gmail.com>
2024-04-16 21:16:52 -0400

    added and tested user backend sign up

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,36 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
-module.exports = router;
+// Sign up
+router.post(
+    '/',
+    async (req, res) => {
+      const { email, password, firstName, lastName, providerBool, phone, imageURL } = req.body;
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({ email, firstName, lastName, providerBool, phone,  hashedPassword, imageURL });
+  
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        providerBool: user.providerBool,
+        phone: user.phone,
+        password: user.password,
+        imageURL: user.imageURL
+      };
+  
+      await setTokenCookie(res, safeUser);
+  
+      return res.json({
+        user: safeUser
+      });
+    }
+  );
+
+  module.exports = router;
