692a027e47d2ca1f3231784a224fc10f58e53535
bssam <bssammel@gmail.com>
2024-04-17 13:41:13 -0400

    debugging missing users table in render

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -2,6 +2,7 @@ const { sequelize } = require('./db/models');
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
+    console.log("psql-setup-script.js is running!")
     await sequelize.createSchema(process.env.SCHEMA);
   }
 });
