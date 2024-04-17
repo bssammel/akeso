68dabe48224851d69a00667be9bdbce240ab1f3a
bssam <bssammel@gmail.com>
2024-04-17 14:27:37 -0400

    adding additional debugging line because I think it is failing here

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -3,8 +3,10 @@ const { sequelize } = require('./db/models');
 console.log("psql-setup-script.js is running! (1)")
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
+  console.log("psql-setup-script.js is running! (2)")
+  console.log(process.env.SCHEMA)
   if (!data.includes(process.env.SCHEMA)) {
-    console.log("psql-setup-script.js is running! 2!")
+    console.log("psql-setup-script.js is running! 3!")
     await sequelize.createSchema(process.env.SCHEMA);
   }
 });
