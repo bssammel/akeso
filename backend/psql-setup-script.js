9954c371df3c17fdfbb3f8983b7ad69b8b7e26cb
bssam <bssammel@gmail.com>
2024-04-17 14:29:49 -0400

    trying brute force

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -5,6 +5,8 @@ console.log("psql-setup-script.js is running! (1)")
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   console.log("psql-setup-script.js is running! (2)")
   console.log(process.env.SCHEMA)
+  // need to figure out if this works, why this data.includes line exists and why it is a not. i know it works on my previous project but something is going wonky here
+  await sequelize.createSchema(process.env.SCHEMA);
   if (!data.includes(process.env.SCHEMA)) {
     console.log("psql-setup-script.js is running! 3!")
     await sequelize.createSchema(process.env.SCHEMA);
