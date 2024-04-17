24e7b1baa8d4bb929353673c2cb9e682477335b0
bssam <bssammel@gmail.com>
2024-04-17 13:46:53 -0400

    still debugging, adding new console.log to try to see where the script is failing

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,8 +1,10 @@
 const { sequelize } = require('./db/models');
 
+console.log("psql-setup-script.js is running! (1)")
+
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
-    console.log("psql-setup-script.js is running!")
+    console.log("psql-setup-script.js is running! 2!")
     await sequelize.createSchema(process.env.SCHEMA);
   }
 });
