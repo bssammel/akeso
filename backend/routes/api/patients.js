d4aabcf0e0293b8e29914e16f04ad536f13081cf
bssam <bssammel@gmail.com>
2024-04-23 20:54:26 -0400

    switching back to traditional create query from the raw sql query

--- backend/routes/api/patients.js
+++ backend/routes/api/patients.js
@@ -219,62 +219,62 @@ router.post(
         const {sex, dob, gender, insurance, religion, relationshipStatus, language, ethnicity, street, city, state, name911, phone911, street911, city911, state911, relationship911, pharmName, pharmStreet, pharmCity, pharmState } = req.body;
         const { user } = req;
         const userId = user.id;
-        // const newPatient = await Patient.create({
-        //     userId, 
-        //     sex,
-        //     dob, 
-        //     gender, 
-        //     insurance, 
-        //     religion, 
-        //     relationshipStatus, 
-        //     language, 
-        //     ethnicity, 
-        //     street, 
-        //     city, 
-        //     state, 
-        //     name911, 
-        //     phone911, 
-        //     street911, 
-        //     city911, 
-        //     state911,
-        //     relationship911, 
-        //     pharmName, 
-        //     pharmStreet, 
-        //     pharmCity, 
-        //     pharmState 
-        // });
+        const newPatient = await Patient.create({
+            userId, 
+            sex,
+            dob, 
+            gender, 
+            insurance, 
+            religion, 
+            relationshipStatus, 
+            language, 
+            ethnicity, 
+            street, 
+            city, 
+            state, 
+            name911, 
+            phone911, 
+            street911, 
+            city911, 
+            state911,
+            relationship911, 
+            pharmName, 
+            pharmStreet, 
+            pharmCity, 
+            pharmState 
+        });
 
         // const newPatient = await sequelize.query(sql`INSERT INTO patients (userId, dob, sex, gender, insurance, religion, relationshipStatus, language, ethnicity, street, city, state, name911, phone911, street911, city911, state911, relationship911, pharmName, pharmStreet, pharmCity, pharmState) VALUES (${userId},${dob},${sex},${gender},${insurance},${religion},${relationshipStatus},${language},${ethnicity},${street},${city},${state},${name911},${phone911},${street911},${city911},${state911},${relationship911},${pharmName},${pharmStreet},${pharmCity},${pharmState})`)
 
-        const newPatient = await db.sequelize.query(`INSERT INTO ${patientTableName} (userId, dob, sex, gender, insurance, religion, relationshipStatus, language, ethnicity, street, city, state, name911, phone911, street911, city911, state911, relationship911, pharmName, pharmStreet, pharmCity, pharmState) VALUES (${userId},${dob},"${sex}","${gender}","${insurance}","${religion}","${relationshipStatus}","${language}","${ethnicity}","${street}","${city}","${state}","${name911}",${phone911},"${street911}","${city911}","${state911}","${relationship911}","${pharmName}","${pharmStreet}","${pharmCity}","${pharmState}")`, {
-            type: QueryTypes.INSERT,
-        });
+        // const newPatient = await db.sequelize.query(`INSERT INTO ${patientTableName} (userId, dob, sex, gender, insurance, religion, relationshipStatus, language, ethnicity, street, city, state, name911, phone911, street911, city911, state911, relationship911, pharmName, pharmStreet, pharmCity, pharmState) VALUES (${userId},${dob},"${sex}","${gender}","${insurance}","${religion}","${relationshipStatus}","${language}","${ethnicity}","${street}","${city}","${state}","${name911}",${phone911},"${street911}","${city911}","${state911}","${relationship911}","${pharmName}","${pharmStreet}","${pharmCity}","${pharmState}")`, {
+        //     type: QueryTypes.INSERT,
+        // });
 
-        console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-        console.log(newPatient)
-        console.log(newPatient[0])
-        const newPtId = newPatient[0];
+        // console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        // console.log(newPatient)
+        // console.log(newPatient[0])
+        // const newPtId = newPatient[0];
         
-        const ptObj = await Patient.findOne({
-            where: {
-                id: newPtId
-            },
-            // include: [
-            //     {
-            //         model: User,
-            //         attributes: [ 
-            //             "firstName", "lastName", "email", "phone"
-            //         ],
-            //     }
-            // ],
-            attributes: [
-                'id', 'userId', 'sex', 'gender', 'insurance', 'religion','relationshipStatus','language', 'ethnicity','street', 'city','state','name911','phone911','street911','city911','state911','relationship911','pharmName','pharmStreet','pharmCity','pharmState'
-            ],
+        // const ptObj = await Patient.findOne({
+        //     where: {
+        //         id: newPtId
+        //     },
+        //     // include: [
+        //     //     {
+        //     //         model: User,
+        //     //         attributes: [ 
+        //     //             "firstName", "lastName", "email", "phone"
+        //     //         ],
+        //     //     }
+        //     // ],
+        //     attributes: [
+        //         'id', 'userId', 'sex', 'gender', 'insurance', 'religion','relationshipStatus','language', 'ethnicity','street', 'city','state','name911','phone911','street911','city911','state911','relationship911','pharmName','pharmStreet','pharmCity','pharmState'
+        //     ],
             
-        })
+        // })
 
-        return res.status(201).json(ptObj)
-        // return res.status(201).json(newPatient)
+        // return res.status(201).json(ptObj)
+        return res.status(201).json(newPatient)
 
 
     })
