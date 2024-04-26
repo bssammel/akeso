4d864f124741f9d11ec308a30f48f39a031902c1
bssam <bssammel@gmail.com>
2024-04-26 04:49:41 -0400

    removing the extra user authentication buttons when a user is at any page and not signed in

--- frontend/src/components/HomePage/UnauthView.jsx
+++ frontend/src/components/HomePage/UnauthView.jsx
@@ -1,15 +1,15 @@
 // import { useSelector} from 'react-redux'
-import LoginFormModal from '../LoginFormPage/LoginFormPage';
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
-import ProviderSignupFormModal from "../RegisterUserPages/ProviderSignupModal";
-import PatientSignupFormModal from "../RegisterUserPages/PatientSignupModal";
+// import LoginFormModal from '../LoginFormPage/LoginFormPage';
+// import OpenModalButton from '../OpenModalButton/OpenModalButton';
+// import ProviderSignupFormModal from "../RegisterUserPages/ProviderSignupModal";
+// import PatientSignupFormModal from "../RegisterUserPages/PatientSignupModal";
 
 function UnauthView(){
     return (
         <div className='unauthed'>
             <h2>Welcome to Akeso!</h2>
             <p>In order to protect our users&apos; privacy, if you would like to view patients, providers, practices, health information, or manage appointments, you must be signed in.</p>
-            <ul>
+            {/* <ul>
                 <li>
                     <OpenModalButton
                         buttonText="Log In"
@@ -28,7 +28,7 @@ function UnauthView(){
                         modalComponent={<PatientSignupFormModal />}
                     />
                 </li>
-            </ul>
+            </ul> */}
         </div>
     )
     
